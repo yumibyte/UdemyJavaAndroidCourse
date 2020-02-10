c997d3eccae99b8ddc41e04e30d85b3e7f777227
Yumibyte <ashraigosa@gmail.com>
2020-02-10 01:41:35 -0800

    data types and stack vs heap memory

--- VariablesInJava/app/src/main/java/com/example/variablesinjava/MainActivity.java
+++ VariablesInJava/app/src/main/java/com/example/variablesinjava/MainActivity.java
@@ -9,5 +9,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //primitive data types, doesn't occupy ^ memory
+        int age = 20;
+        float price = 1.5f;
+        double userScore = 4.245245;
+
+        long computerRam = 12323423423L;
+
+        boolean isDead = true;
+
+        String name = "Morteza";
+
+        char letterA = 'A';
+        // Stack memory variable is ASSIGNED the value (primitives like int)
+        // Heap memory is where objects are stored. Reference type. Stack will only hold the address.
+
+
     }
 }
